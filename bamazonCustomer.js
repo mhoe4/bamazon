ef320373b9b8e530136ac9188949692af4575553
mhoef <mhoefer1@gmail.com>
2019-02-17 00:36:39 -0500

    bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,4 @@
-// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
-// 6. The app should then prompt users with two messages.
-
-//    * The first should ask them the ID of the product they would like to buy.
-//    * The second message should ask how many units of the product they would like to buy.
-
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//    * This means updating the SQL database to reflect the remaining quantity.
-//    * Once the update goes through, show the customer the total cost of their purchase.
+"use strict"
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -89,7 +76,9 @@ function checkUnits(numberUnits, item_id) {
 
         //console.log(parseInt(res[0].stock_quantity) - numberUnits);
         var total = res[0].price*numberUnits;
-        updateStock((res[0].stock_quantity - numberUnits), item_id, total);
+        console.log(`Remaining Units: ${(res[0].stock_quantity - numberUnits)}`);
+        console.log(`Total Price = $${total}`); 
+        updateStock((res[0].stock_quantity - numberUnits), item_id);
       }
       else {
 
@@ -122,13 +111,9 @@ function redoNumberUnits(item_id) {
     });
 }
 
-function updateStock(remainingUnits, item_id, total) {
-  console.log(`Remaining Units: ${remainingUnits}`);
+function updateStock(remainingUnits, item_id) {
   var query = `UPDATE products SET stock_quantity = ${remainingUnits} WHERE item_id =  ${item_id};`;
   connection.query(query, function (err, res) {
-    
-    console.log(`Total Price = $${total}`); 
-    
     console.log('Thanks! Have a Wonderful Day!');
     connection.end();
   });
